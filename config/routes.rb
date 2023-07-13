74d08f92f09388b7d227211637e479bd42782ece
developer <shpandey@bestpeers.com>
2023-07-13 20:18:11 +0530

    Added user book method and jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users      
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "books#index"
   resources :books
+  resources :user_books, only: [:index]
+  post '/borrow_book', to: 'user_books#borrow_book', as: 'borrow_book'
+  patch '/return_book', to: 'user_books#return_book', as: 'return_book'
 end
